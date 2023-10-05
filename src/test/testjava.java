b9b1a9960cfd7f1e3bab4b260bcaa27a07c6f298
aborjas <aborjas@STF-MCK-BORJAS.whitworth.edu>
2023-10-04 21:13:03 -0700

    Added not statement

--- src/test/testjava.java
+++ src/test/testjava.java
@@ -8,12 +8,18 @@ public class testjava {
 	 Scanner scanner = new Scanner(System.in);	
 
 	System.out.println("Hello World!");
-	
+	//Implement the classic rock, paper, scissors game. 
+	//Ask the user for a 1 (rock), 2 (paper), or 3 (scissors). 
+	//Then generate a random number between 1 and 3 using the random class. 
+	//Output the user's choice and your choice, 
+	//then tell the user who won.
 	Random random = new Random();  
 	System.out.print("Pick 1 for rock, 2 for paper, 3 for scissors");
 	int comp = random.nextInt()%3+1;
 	int user = scanner.nextInt();
-	
+	if ((user !=1) && (user !=2) && (user !=3)); {
+		System.out.print("Invalid input. You loose!");
+	}
 	
 
 	}
