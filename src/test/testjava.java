7de5a196a4f161e63e82617ccee005a23e868c8f
aborjas <aborjas@STF-MCK-BORJAS.whitworth.edu>
2023-10-05 06:57:31 -0700

    Added full if statement, needs work

--- src/test/testjava.java
+++ src/test/testjava.java
@@ -17,10 +17,19 @@ public class testjava {
 	System.out.print("Pick 1 for rock, 2 for paper, 3 for scissors");
 	int comp = random.nextInt()%3+1;
 	int user = scanner.nextInt();
-	if ((user !=1) && (user !=2) && (user !=3)); {
+	//If statements
+	if ((user !=1) && (user !=2) && (user !=3)); { //Condition 1
 		System.out.print("Invalid input. You loose!");
+	} 
+	else (user == comp) { //Condition 2
+		System.out.println("tie");
+		}
+		if ((comp == 1 && user == 3) || (comp == 2 && user == 1) || (comp == 3 && user  == 2) ) {
+			System.out.println("The computer won!");
+		}
+			else 
+				System.out.println("You won!");
 	}
-	
 
 	}
-}
+
