0c4d7f3dc848ab7d027175e65c9403c8ffda2319
aborjas <aborjas@STF-MCK-BORJAS.whitworth.edu>
2023-10-05 18:47:03 -0700

    Completed if else statements

--- src/test/testjava.java
+++ src/test/testjava.java
@@ -5,31 +5,31 @@ import java.util.Scanner;
 public class testjava { 
  
  public static void main(String[] args) {
-	 Scanner scanner = new Scanner(System.in);	
-
-	System.out.println("Hello World!");
-	//Implement the classic rock, paper, scissors game. 
-	//Ask the user for a 1 (rock), 2 (paper), or 3 (scissors). 
-	//Then generate a random number between 1 and 3 using the random class. 
-	//Output the user's choice and your choice, 
-	//then tell the user who won.
+	Scanner scanner = new Scanner(System.in);	
+	
 	Random random = new Random();  
 	System.out.print("Pick 1 for rock, 2 for paper, 3 for scissors");
 	int comp = random.nextInt()%3+1;
 	int user = scanner.nextInt();
+	
 	//If statements
-	if ((user !=1) && (user !=2) && (user !=3)); { //Condition 1
-		System.out.print("Invalid input. You loose!");
+	//Does the user enter 1, 2 or 3?
+	if ((user !=1) && (user !=2) && (user !=3)) {
+		System.out.println("Invalid input. You loose!");
 	} 
-	else (user == comp) { //Condition 2
-		System.out.println("tie");
+	else { //The user enters 1, 2, or 3. Evaluate conditions
+		if (user == comp) { //Condition 1: numbers match
+			System.out.println("tie");
+		} 
+			else if ((comp == 1 && user == 3) || (comp == 2 && user == 1) || (comp == 3 && user  == 2) ) {//Condition 2: computer beats user
+				System.out.println("The computer won!"); 
+		} 
+		else { //Condition 3: doesn't meet either criteria above, so user has entered a number that beats the computer
+			System.out.println("You won!"); 
 		}
-		if ((comp == 1 && user == 3) || (comp == 2 && user == 1) || (comp == 3 && user  == 2) ) {
-			System.out.println("The computer won!");
 		}
-			else 
-				System.out.println("You won!");
 	}
 
-	}
+}
+
 
