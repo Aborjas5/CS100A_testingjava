a80e909b7bdb573aa97704e7753175d584cf0fe1
aborjas <aborjas@STF-MCK-BORJAS.whitworth.edu>
2023-10-07 16:10:49 -0700

    Adding for loop to rock paper scissors game

--- src/test/testjava.java
+++ src/test/testjava.java
@@ -1,6 +1,8 @@
 package test;
 
 import java.util.Random;  
+
+
 import java.util.Scanner;	
 public class testjava { 
  
@@ -8,10 +10,24 @@ public class testjava {
 	Scanner scanner = new Scanner(System.in);	
 	
 	Random random = new Random();  
+	
+	//Using for loop with rock, paper, scissors
+	System.out.print("Pick 1 for rock, 2 for paper, 3 for scissors"); //User asked to pick a number
+	int comp = random.nextInt()%3+1; // Computer selects a random number between 1-3
+	int user = scanner.nextInt(); // User number entered above named here
+	
+	//introduce for loop
+	for (int i =0; i??, i++) {
+	
+	
+	
+	
+	//Original code for rock, paper, scissors)
 	System.out.print("Pick 1 for rock, 2 for paper, 3 for scissors");
 	int comp = random.nextInt()%3+1;
 	int user = scanner.nextInt();
-	
+
+
 	//If statements
 	//Does the user enter 1, 2 or 3?
 	if ((user !=1) && (user !=2) && (user !=3)) {
