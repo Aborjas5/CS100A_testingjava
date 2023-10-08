e3b12c5fade122802fe5d306c902c8846694d980
aborjas <aborjas@STF-MCK-BORJAS.whitworth.edu>
2023-10-08 09:48:01 -0700

    While lool for Rock paper scissors - won't stop looping

--- src/test/testjava.java
+++ src/test/testjava.java
@@ -1,6 +1,7 @@
 package test;
 
-import java.util.Random;  
+import java.util.Random; 
+import java.util.Scanner; 
 
 
 import java.util.Scanner;	
@@ -8,30 +9,21 @@ public class testjava {
  
  public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);	
-	
 	Random random = new Random();  
 	
-	//Using for loop with rock, paper, scissors
+	//Using while loop with rock, paper, scissors
 	System.out.print("Pick 1 for rock, 2 for paper, 3 for scissors"); //User asked to pick a number
-	int comp = random.nextInt()%3+1; // Computer selects a random number between 1-3
-	int user = scanner.nextInt(); // User number entered above named here
+	int user = scanner.nextInt(); // User number named here
+	int userWins = 0;
+	int computerWins = 0;
 	
-	//introduce for loop
-	for (int i =0; i??, i++) {
-	
-	
-	
-	
-	//Original code for rock, paper, scissors)
-	System.out.print("Pick 1 for rock, 2 for paper, 3 for scissors");
-	int comp = random.nextInt()%3+1;
-	int user = scanner.nextInt();
+	//introduce while loop
+	while (true) {
+		int comp = random.nextInt()%3+1; // Computer selects a random number between 1-3
 
-
-	//If statements
-	//Does the user enter 1, 2 or 3?
 	if ((user !=1) && (user !=2) && (user !=3)) {
 		System.out.println("Invalid input. You loose!");
+			continue;
 	} 
 	else { //The user enters 1, 2, or 3. Evaluate conditions
 		if (user == comp) { //Condition 1: numbers match
@@ -43,9 +35,16 @@ public class testjava {
 		else { //Condition 3: doesn't meet either criteria above, so user has entered a number that beats the computer
 			System.out.println("You won!"); 
 		}
+			
+	if (userWins >= 1) {
+		System.out.print("Congratulations! You've won at least one time!");
+		break; 
+	}
+		}
 		}
 	}
+ }
 
-}
+
 
 
